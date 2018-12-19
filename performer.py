8e5e454ec52c1f97971d93ce112d4a40a95d8ed5
XuSShuai <18235417377@163.com>
2018-12-19 22:21:42 +0800

    modify

--- performer.py
+++ performer.py
@@ -5,12 +5,12 @@ import numpy as np
 data_set_name = ["mutag", "proteins", "cni1", "dd"]
 performance = {}
 for name_file in data_set_name:
-    main.parse_args(name_file)
+    args = main.parse_args(name_file)
     if name_file not in performance.keys():
         performance[name_file] = []
     for i in range(10):
         print("-" * 30, name_file, "test: ", i)
-        performance[name_file].append(main.main())
+        performance[name_file].append(main.gnn(args))
 print()
 for key, value in performance.items():
     with open(os.path.join("logs", key+".txt"), "a") as f:
